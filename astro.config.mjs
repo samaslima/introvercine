816c98407b21ac2acf5a0c4bfcb8cb0724df6a67
Yasser Nascimento <dev@yasser.email>
2025-07-16 06:34:14 -0300

    Add Astropod

---
+++ astro.config.mjs
@@ -0,0 +1,34 @@
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+import NetlifyCMS from "astro-netlify-cms";
+import astropodConfig from "./.astropod/astropod.config.json";
+import robotsTxt from "astro-robots-txt";
+
+// https://astro.build/config
+import image from "@astrojs/image";
+
+// https://astro.build/config
+export default defineConfig({
+  site: astropodConfig.site,
+  integrations: [
+    robotsTxt({
+      policy: [
+        {
+          userAgent: "*",
+          allow: "/",
+          disallow: "/admin",
+        },
+      ],
+    }),
+    mdx(),
+    sitemap(),
+    tailwind(),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+      cacheDir: "./.cache/image",
+      logLevel: "debug",
+    }),
+  ],
+});
