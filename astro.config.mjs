a8e2db5aca3ace8dd35c58d2e4f204405fd791fc
Yasser Nascimento <dev@yasser.email>
2025-07-16 09:51:28 -0300

    Clean `@astrojs/image` deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,22 +2,10 @@ import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
-import astropodConfig from './.astropod/astropod.config.json'
-
-// https://astro.build/config
-import image from '@astrojs/image'
+import { site } from './.astropod/astropod.config.json'
 
 // https://astro.build/config
 export default defineConfig({
-  site: astropodConfig.site,
-  integrations: [
-    mdx(),
-    sitemap(),
-    tailwind(),
-    image({
-      serviceEntryPoint: '@astrojs/image/sharp',
-      cacheDir: './.cache/image',
-      logLevel: 'debug',
-    }),
-  ],
+  site,
+  integrations: [mdx(), sitemap(), tailwind()],
 })
