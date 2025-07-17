d10bd2876ccb2eca79da09844dfb0bd9b1c69622
Yasser Nascimento <dev@yasser.email>
2025-07-17 09:16:28 -0300

    Remove `@astrojs/mdx` and `@astrojs/rss` dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,4 @@
 import { defineConfig } from 'astro/config'
-import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
 import { site } from './.astropod/astropod.config.json'
@@ -7,5 +6,5 @@ import { site } from './.astropod/astropod.config.json'
 // https://astro.build/config
 export default defineConfig({
   site,
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [sitemap(), tailwind()],
 })
